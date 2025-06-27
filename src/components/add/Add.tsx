ffc70e36896a107fc237730a13cdb13b08e62197
Mani <mnmnkandan@gmail.com>
2025-06-27 16:13:52 +0530

    add file changes

--- src/components/add/Add.tsx
+++ src/components/add/Add.tsx
@@ -1,6 +1,6 @@
 import type { GridColDef } from "@mui/x-data-grid";
 import "./add.scss";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+
 
 type Props = {
   slug: string;
@@ -10,34 +10,7 @@ type Props = {
 
 const Add = (props: Props) => {
 
-  // TEST THE API
-
-  // const queryClient = useQueryClient();
-
-  // const mutation = useMutation({
-  //   mutationFn: () => {
-  //     return fetch(`http://localhost:8800/api/${props.slug}s`, {
-  //       method: "post",
-  //       headers: {
-  //         Accept: "application/json",
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({
-  //         id: 111,
-  //         img: "",
-  //         lastName: "Hello",
-  //         firstName: "Test",
-  //         email: "testme@gmail.com",
-  //         phone: "123 456 789",
-  //         createdAt: "01.02.2023",
-  //         verified: true,
-  //       }),
-  //     });
-  //   },
-  //   onSuccess: () => {
-  //     queryClient.invalidateQueries([`all${props.slug}s`]);
-  //   },
-  // });
+ 
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
